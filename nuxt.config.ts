00777d204c534f0f2538a2466d0642f1b3df0323
rcluis <raulcatalanluis@gmail.com>
2023-05-21 18:22:45 +0200

    fix: service account by env variables

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -34,9 +34,6 @@ export default defineNuxtConfig({
   ],
   vuefire: {
     auth: true,
-    admin: {
-      serviceAccount: './serviceAccount.json'
-    },
     config: {
       apiKey: process.env.FIREBASE_API_KEY,
       authDomain: process.env.FIREBASE_AUTH_DOMAIN,
