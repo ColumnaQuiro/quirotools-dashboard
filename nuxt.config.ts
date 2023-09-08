de90f2d7f63416129c42d99cf63feb58e2172153
rcluis <raulcatalanluis@gmail.com>
2023-09-08 09:45:31 +0200

    added: hubspot live chat

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,10 @@ export default defineNuxtConfig({
   app: {
     head: {
       charset: 'utf-8',
-      viewport: 'width=device-width, initial-scale=1.0'
+      viewport: 'width=device-width, initial-scale=1.0',
+      script: [
+        { type: 'text/javascript', id: 'hs-script-loader', async: true, defer: true, src: '//js-eu1.hs-scripts.com/143268808.js' }
+      ]
     }
   },
   nitro: {
